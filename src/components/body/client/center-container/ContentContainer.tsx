37e4995c78dd53aa218ee8cc9677c6472605e400
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2025-02-25 00:08:38 +0100

    [FEATURE] Client form bar implemented.

--- src/components/body/client/center-container/ContentContainer.tsx
+++ src/components/body/client/center-container/ContentContainer.tsx
@@ -1,9 +1,36 @@
+import { useState } from "react";
+
 import './ContentContainer.css'
+import { MethodSelector } from "./method-selector/MethodSelector";
 
 export function ContentContainer() {
+    const [formData, setFormData] = useState({
+        url: "",
+        method: ""
+      });
+    
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleMethodChange = (method: string) => {
+        setFormData({ ...formData, method });
+    };
+
+    const handleSubmit = (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+        console.log("Form Data Submitted:", formData);
+    };
+
     return (
         <div id='content-container'>
-
+            <form id="client-form" onSubmit={handleSubmit}>
+                <div id="client-bar">
+                    <MethodSelector selected={formData.method} onMethodChange={handleMethodChange}/>
+                    <input id="url" className="client-bar-component" name="url" type="text" onChange={handleChange} value={formData.url}/>
+                    <button id="client-button-send" className="client-bar-component">Send</button>
+                </div>
+            </form>
         </div>
     )
 }
---
+++ src/components/body/client/center-container/method-selector/MethodSelector.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+import './MethodSelector.css'
+import { HttpMethod } from '../../../../../constants/HttpMethod';
+
+interface MethodSelectorProps {
+    selected?: string;
+    onMethodChange: (method: string) => void; 
+}
+
+export function MethodSelector ({selected, onMethodChange }: MethodSelectorProps) {
+    const methods = Object.values(HttpMethod);
+    const [selectedMethod, setSelectedMethod] = useState(selected || methods[0]);
+
+    const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectedMethod(e.target.value);
+        onMethodChange(e.target.value)
+    };
+
+    return (
+        <select id="method" className="client-bar-component" name="method" value={selectedMethod} onChange={handleChange}>
+            {methods.map((method, index) => (
+                <option key={index} value={method}>
+                    {method}
+                </option>
+            ))}
+        </select>
+    )
+}
