2dbdb527453f7d95f618cb87fdc125feaa5f2c99
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2025-03-01 11:29:18 +0100

    [FEATURE] Codemirror implemented.

--- src/components/body/client/center-container/client-arguments/body-arguments/BodyArguments.tsx
+++ src/components/body/client/center-container/client-arguments/body-arguments/BodyArguments.tsx
@@ -1,9 +1,29 @@
+import { useState } from 'react';
+
+import CodeMirror from "@uiw/react-codemirror";
+import { json, jsonParseLinter  } from "@codemirror/lang-json";
+import { linter, lintGutter } from "@codemirror/lint";
+
 import './BodyArguments.css'
 
 export function BodyArguments() {
+    const [code, setCode] = useState("{}");
+    
+      const onChange = (value: string) => {
+        setCode(value);
+        console.log("Updated code:", value);
+      };
+
     return (
         <>
             <p>//TODO: Implement BodyArguments</p>
+            <CodeMirror
+                value={code}
+                height="300px"
+                extensions={[json(), linter(jsonParseLinter()), lintGutter()]}
+                onChange={onChange}
+                theme="light"
+            />
         </>
     )
 }
