62d69ecb6713548ad964e021b8629e7276416142
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2025-03-23 23:36:04 +0100

    [FEATURE] Add temporal remove button to stored item.

--- src/components/body/client/left-sidebar/stored-column/StoredColumn.tsx
+++ src/components/body/client/left-sidebar/stored-column/StoredColumn.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useImperativeHandle, useState } from 'react';
-import { findAllAction } from '../../../../../services/api/ServiceStorage';
+import { deleteAction, findAllAction } from '../../../../../services/api/ServiceStorage';
 import { newRequest, Request } from '../../../../../interfaces/request/Request';
 import { millisecondsToDate } from '../../../../../services/Tools';
 
@@ -23,6 +23,17 @@ export function StoredColumn({ ref, selected, defineRequest, selectRequest }: St
         fetchStored
     }));
     
+    
+    useEffect(() => {
+        fetchStored();
+
+        const interval = setInterval(() => {
+            fetchStored();
+        }, 10000);
+
+        return () => clearInterval(interval);
+    }, []);
+
     const fetchStored = async () => {
         try {
             const data = (await findAllAction("anonymous"))
@@ -33,15 +44,14 @@ export function StoredColumn({ ref, selected, defineRequest, selectRequest }: St
         }
     };
 
-    useEffect(() => {
-        fetchStored();
-
-        const interval = setInterval(() => {
-            fetchStored();
-        }, 10000);
-
-        return () => clearInterval(interval);
-    }, []);
+    const deleteStored = async (request: Request) => {
+        try {
+            await deleteAction("anonymous", request);
+            await fetchStored();
+        } catch (error) {
+            console.error("Error fetching history:", error);
+        }
+    };
 
     const makeKey = (request: Request): string => {
         return `${request.timestamp}-${request.method}-${request.uri}`;
@@ -71,8 +81,10 @@ export function StoredColumn({ ref, selected, defineRequest, selectRequest }: St
                                         </div>
                                     </a>
                                     <button 
-                                        type="button"
-                                        >*</button>
+                                        type="button" 
+                                        className="remove-button show"
+                                        onClick={() => deleteStored(request)}>
+                                    </button>
                                 </div>
                             ))
                         ) : (
