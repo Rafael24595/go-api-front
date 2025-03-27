bc64b41926eac07a88f4b711fd879b22b7df7393
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2025-03-27 20:05:51 +0100

    [FEATURE] Implement Alert component.

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import './App.css'
 import { Body } from './components/body/Body.tsx'
 import { Footer } from './components/footer/Footer.tsx'
 import { Header } from './components/header/Header.tsx'
+import { Alert } from './components/utils/alert/Alert.tsx'
 
 function App() {
   return (
     <>
-      <Header/>
-      <Body/>
-      <Footer/>
+      <Alert>
+        <Header/>
+        <Body/>
+        <Footer/>
+      </Alert>
     </>
   )
 }
