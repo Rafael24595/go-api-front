e139c7417f9867639c406b956c7714e64a244ae3
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2026-01-05 20:13:11 +0100

    feat: add html preview

--- src/components/body/client/right-sidebar/payload-column/PayloadColumn.tsx
+++ src/components/body/client/right-sidebar/payload-column/PayloadColumn.tsx
@@ -12,10 +12,12 @@ import { copyTextToClipboard } from '../../../../../services/Utils';
 import { useAlert } from '../../../../utils/alert/Alert';
 import { EAlertCategory } from '../../../../../interfaces/AlertData';
 import { formatHtml, formatJson, formatXml } from '../../../../../utils/Formatter';
+import { booleanParser } from '../../../../../store/Helper';
 
 import './PayloadColumn.css';
 
 const STATUS_KEY = "PayloadColumnFormatStatus";
+const HTML_RENDER_KEY = "PayloadColumnHtmlRender";
 
 const VIEW_TEXT = "text";
 const VIEW_JSON = "json";
@@ -29,25 +31,30 @@ interface Payload {
     format: string;
 }
 
+interface HtmlViewPayload {
+    render: boolean;
+}
+
 export function PayloadColumn() {
     const { find, store } = useStoreStatus();
-    
+
     const { push } = useAlert();
 
     const { response } = useStoreRequest();
 
     const [data, setData] = useState<Payload>(() => {
-        const autoFormat = find(STATUS_KEY, {
-            def: true,
-            parser: (v) => v == "true"
-        });
-        const format = autoFormat 
-            ? detectLang(response.body.payload) 
+        const autoFormat = find(STATUS_KEY, booleanParser());
+        const format = autoFormat
+            ? detectLang(response.body.payload)
             : response.body.content_type;
         return {
             autoFormat,
             format
         }
+    });
+
+    const [htmlViewData, setHtmlViewData] = useState<HtmlViewPayload>({
+        render: find(HTML_RENDER_KEY, booleanParser())
     });
 
     useEffect(() => {
@@ -56,37 +63,37 @@ export function PayloadColumn() {
 
     const copyPayloadToClipboard = async (text: string) => {
         text = await format(text);
-        copyTextToClipboard(text, 
+        copyTextToClipboard(text,
             () => push({
-                    category: EAlertCategory.INFO,
-                    content: "The payload content has been copied to the clipboard"
-                }),
+                category: EAlertCategory.INFO,
+                content: "The payload content has been copied to the clipboard"
+            }),
             (err) => push({
-                    category: EAlertCategory.ERRO,
-                    content:`The payload content could not be copied to the clipboard: ${err.message}`
-                }),
+                category: EAlertCategory.ERRO,
+                content: `The payload content could not be copied to the clipboard: ${err.message}`
+            }),
         );
     }
 
     const format = async (text: string) => {
-        if(cusorIs(VIEW_TEXT)) {
+        if (cusorIs(VIEW_TEXT)) {
             return text;
         }
-        
+
         try {
-            if(cusorIs(VIEW_JSON)) {
+            if (cusorIs(VIEW_JSON)) {
                 return await formatJson(text);
             }
-            if(cusorIs(VIEW_XML)) {
+            if (cusorIs(VIEW_XML)) {
                 return await formatXml(text);
             }
-            if(cusorIs(VIEW_HTML)) {
+            if (cusorIs(VIEW_HTML)) {
                 return await formatHtml(text);
             }
         } catch (err) {
             console.error(err);
         }
-        
+
         return text;
     }
 
@@ -100,8 +107,8 @@ export function PayloadColumn() {
     }
 
     const updateFormat = (autoFormat: boolean) => {
-        const format = autoFormat 
-            ? detectLang(response.body.payload) 
+        const format = autoFormat
+            ? detectLang(response.body.payload)
             : response.body.content_type;
         setData((prevData) => ({
             ...prevData,
@@ -110,39 +117,50 @@ export function PayloadColumn() {
     }
 
     const cusorIs = (cursor: string) => {
-        if(!data.autoFormat || data.format == VIEW_TEXT) {
+        if (!data.autoFormat || data.format == VIEW_TEXT) {
             return response.body.content_type === cursor;
         }
         return data.format === cursor;
+    }
+
+    const switchHtmlRender = () => {
+        setHtmlViewData(prevData => ({
+            render: !prevData.render
+        }));
     }
 
     return (
         <>
             <div id="response-payload-bytes">
                 <div id="response-content-types">
-                    <div id="detect-container">
-                        <label htmlFor="detect-format" className="select-none">Detect: </label>
-                        <input 
-                            name="status" 
-                            id="detect-format" 
-                            type="checkbox" 
-                            checked={data.autoFormat}
-                            onChange={autoFormatChange}/>
-                    </div>
-                    <span className="select-none">{ viewParse(response.body.content_type) }</span>
+                    {response.body.content_type == "html" ? (
+                        <button className="modal-button" onClick={switchHtmlRender}>{ htmlViewData.render ? "Render" : "Plain" }</button>
+                    ) : (
+
+                        <div id="detect-container">
+                            <label htmlFor="detect-format" className="select-none">Detect: </label>
+                            <input
+                                name="status"
+                                id="detect-format"
+                                type="checkbox"
+                                checked={data.autoFormat}
+                                onChange={autoFormatChange} />
+                        </div>
+                    )}
+                    <span className="select-none">{viewParse(response.body.content_type)}</span>
                 </div>
-                { response.body.payload.length > 0 && (
-                    <button 
-                        className="clipboard-button" 
-                        type="button" 
+                {response.body.payload.length > 0 && (
+                    <button
+                        className="clipboard-button"
+                        type="button"
                         onClick={() => copyPayloadToClipboard(response.body.payload)}>
                     </button>
                 )}
                 <div id="response-content-bytes" className="grid">
-                    {cusorIs(VIEW_TEXT) && <TextView value={response.body.payload}/>}
-                    {cusorIs(VIEW_JSON) && <JsonView value={response.body.payload}/>}
-                    {cusorIs(VIEW_XML) && <XmlView value={response.body.payload}/>}
-                    {cusorIs(VIEW_HTML) && <HtmlView value={response.body.payload}/>}
+                    {cusorIs(VIEW_TEXT) && <TextView value={response.body.payload} />}
+                    {cusorIs(VIEW_JSON) && <JsonView value={response.body.payload} />}
+                    {cusorIs(VIEW_XML) && <XmlView value={response.body.payload} />}
+                    {cusorIs(VIEW_HTML) && <HtmlView value={response.body.payload} render={htmlViewData.render} />}
                 </div>
             </div>
         </>
@@ -153,8 +171,8 @@ hljs.registerLanguage('json', json);
 hljs.registerLanguage('xml', xml);
 
 function detectLang(code: string) {
-  const result = hljs.highlightAuto(code, ['json', 'html', 'xml']);
-  return result.language || VIEW_TEXT;
+    const result = hljs.highlightAuto(code, ['json', 'html', 'xml']);
+    return result.language || VIEW_TEXT;
 }
 
 function viewParse(view: string): string {
